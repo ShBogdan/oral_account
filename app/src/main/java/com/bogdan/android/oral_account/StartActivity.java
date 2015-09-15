ff94dda412991f93ab7c4d3b1be316d9997d4515
ShBogdan <Infocom911>
2015-09-15 12:51:23 +0300

    Add like button

--- app/src/main/java/com/bogdan/android/oral_account/StartActivity.java
+++ app/src/main/java/com/bogdan/android/oral_account/StartActivity.java
@@ -2,16 +2,19 @@ package com.bogdan.android.oral_account;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 
 public class StartActivity extends Activity {
+    String appPackageName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_activity);
+        appPackageName = getApplication().getPackageName();
     }
 
     public void btnInfo(View v){
@@ -25,6 +28,11 @@ public class StartActivity extends Activity {
     }
 
     public  void btnLike(View v){
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
 
     }
 }
